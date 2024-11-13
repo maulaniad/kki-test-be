28d589396036068ee011478b950458ec72161c52
Adrian Maulani <maulani.ad@gmail.com>
2024-11-13 17:22:23 +0700

    feat(book): implement search and ordering

--- api/book/views.py
+++ api/book/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from api.book.serializers import BookSerializer
 from database.models import Book
@@ -9,6 +10,9 @@ class BookListView(ListCreateAPIView):
     pagination_class = StandardPagination
     serializer_class = BookSerializer
     queryset = Book.objects.all().order_by('-created_at')
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['title']
+    ordering_fields = ['title', 'author', 'created_at']
 
 
 class BookDetailView(RetrieveUpdateDestroyAPIView):
