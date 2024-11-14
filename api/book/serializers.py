01f97021d64b9662c9e2a19a07aa0c56c488eb48
Adrian Maulani <maulani.ad@gmail.com>
2024-11-14 21:30:27 +0700

    feat: changed date formatting to dd-mm-yyyy

--- api/book/serializers.py
+++ api/book/serializers.py
@@ -1,4 +1,7 @@
-from rest_framework.serializers import ALL_FIELDS, ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import (ALL_FIELDS,
+                                        ModelSerializer,
+                                        PrimaryKeyRelatedField,
+                                        DateTimeField)
 
 from api.category.serializers import CategorySerializer
 from database.models import Book, Category
@@ -6,6 +9,9 @@ from database.models import Book, Category
 
 class BookSerializer(ModelSerializer):
     category = PrimaryKeyRelatedField(queryset=Category.objects.all())
+    created_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
+    updated_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
+    deleted_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
 
     class Meta:
         model = Book
--- api/category/serializers.py
+++ api/category/serializers.py
@@ -1,9 +1,13 @@
-from rest_framework.serializers import ALL_FIELDS, ModelSerializer
+from rest_framework.serializers import ALL_FIELDS, ModelSerializer, DateTimeField
 
 from database.models import Category
 
 
 class CategorySerializer(ModelSerializer):
+    created_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
+    updated_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
+    deleted_at = DateTimeField(format="%d-%m-%Y %H:%M:%S", read_only=True)
+
     class Meta:
         model = Category
         fields = ALL_FIELDS
